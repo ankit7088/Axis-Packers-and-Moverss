21b61700a49e20fbacd3716001e7ba5442f369df
Ankit Kumar <0404ankitkumar@gmail.com>
2023-08-05 13:07:53 +0530

    inject google meta tag in head section

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ export default function Home() {
         <title>Axis Packers and Movers</title>
         <meta name="Professional Packers and Movers Services | Reliable Moving Company" content="Welcome to Axis Packers and Movers, a trusted and experienced Packers and Movers company offering reliable and efficient moving services. Our skilled team ensures a hassle-free and secure relocation for residential and commercial clients. Contact us now for a seamless moving experience." />
         <link rel="icon" href="/axis.png" />
+        <meta name="google-site-verification" content="so2cK1xxu03nhx9kdXOkG9vQ-GhYsoqRUJCYz9n59mY" />
       </Head>
   <HomeMain/>
     </div>
