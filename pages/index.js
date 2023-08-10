b8604ade4bc7385d8045e444617ec16c96d3593c
Ankit Kumar <0404ankitkumar@gmail.com>
2023-08-10 11:58:44 +0530

    gurugram added in home page

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ export default function Home() {
   return (
     <div className='relative'>
       <Head>
-        <title>Axis Packers and Movers</title>
-        <meta name="Professional Packers and Movers Services | Reliable Moving Company" content="Welcome to Axis Packers and Movers, a trusted and experienced Packers and Movers company offering reliable and efficient moving services. Our skilled team ensures a hassle-free and secure relocation for residential and commercial clients. Contact us now for a seamless moving experience." />
+        <title>Axis Packers and Movers Gurugram</title>
+        <meta name="Professional Packers and Movers Services in Gurugram| Reliable Moving Company" content="Welcome to Axis Packers and Movers Gurugram, a trusted and experienced Packers and Movers company offering reliable and efficient moving services in gurugram. Our skilled team ensures a hassle-free and secure relocation for residential and commercial clients. Contact us now for a seamless moving experience." />
         <meta name="google-site-verification" content="so2cK1xxu03nhx9kdXOkG9vQ-GhYsoqRUJCYz9n59mY" />
         <link rel="icon" href="/axis_logo.png" />
         
